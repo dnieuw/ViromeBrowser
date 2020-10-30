f19c5fef5df6a1868f9f372c287ccdb0f99fd04c
David Nieuwenhuijse <davidnieuwenhuijse@gmail.com>
2020-10-30 09:17:21 +0100

    Removed a bug which caused the heatmap to display the wrong colors and added a selection button to choose which annotation to show

--- inst/app/heatmap_panel.R
+++ inst/app/heatmap_panel.R
@@ -26,12 +26,36 @@ output$metadata_stratify_selection <- renderUI({
   )
 })
 
+output$annotation_stratify_selection <- renderUI({
+  
+  annotation_levels <- c("superkingdom",
+                         "kingdom",
+                         "class",
+                         "order",
+                         "family",
+                         "genus",
+                         "species",
+                         "taxonomic_name")
+  
+  available_annotations <- annotation_levels[annotation_levels %in% colnames(annotation_data())]
+  
+  pickerInput(
+    inputId = "annotation_stratify_selection",
+    label = "Annotate with:", 
+    choices = available_annotations,
+    options =  list(
+      size = 5
+    ), 
+    multiple = FALSE
+  )
+})
+
 annot.heatmap.data <- eventReactive({annotation_data(); input$update_heatmap}, ignoreNULL = FALSE, {
   validate(
     need(!is.null(annotation_data()),"Loading..."),
     need(!is.null(metadata()),"Loading...")
   )
-
+  
   contig_data <- annotation_data()
   
   #If no selection is made based on contig data set the default thresholds
@@ -74,7 +98,7 @@ annot.heatmap.data <- eventReactive({annotation_data(); input$update_heatmap}, i
 output$annot.heatmap <- renderRbokeh({
 	
   x_axis <- req(input$metadata_stratify_selection)
-  y_axis <- "annotation"
+  y_axis <- req(input$annotation_stratify_selection)
   
   #Sum contig by stratification
 	plotdata <- annot.heatmap.data()[,.N, by=.(get(x_axis),get(y_axis))]
@@ -93,7 +117,7 @@ output$annot.heatmap <- renderRbokeh({
 		tools = c("box_select", "save"),
 		toolbar_location = "above") %>%
 		theme_legend(background_fill_alpha = 0, border_line_alpha = 0) %>%
-		ly_crect(data = plotdata, x=get(x_axis), y=get(y_axis), color=Contigs, hover=list(x_axis=get(x_axis),y_axis=get(y_axis),"#Contigs"=N), lname = "rects") %>%
+		ly_crect(data = plotdata, x=get(x_axis), y=get(y_axis), color=N, hover=list(x_axis=get(x_axis),y_axis=get(y_axis),"#Contigs"=N), lname = "rects") %>%
 		x_range(x.axis) %>%
 		theme_axis("x", major_label_orientation = 45) %>%
 		theme_grid("x", grid_line_alpha = 0) %>%
@@ -104,30 +128,30 @@ output$annot.heatmap <- renderRbokeh({
 })
 
 #Generate a table with contig info
-output$contig_table <- DT::renderDataTable({
+output$contig_table <- renderDataTable({
   validate(
     need(!is.null(annotation_data()),"Loading...")
   )
   plotdata <- annotation_data()[,c(-1,-2), with=FALSE]
-  DT::datatable(plotdata,
+  datatable(plotdata,
             filter = 'top',
             options = list(
               dom = "t",
               pageLength = 0,
-              sScrollY = "0px",
+              # sScrollY = "0px",
               autoWidth = TRUE
             )
   )
 })
 
 #Generate a table with metadata info
-output$metadata_table <- DT::renderDataTable({
+output$metadata_table <- renderDataTable({
   validate(
     need(!is.null(metadata()),"Loading...")
   )
   req(input$metadata_filter_selection)
   plotdata <- metadata()[,input$metadata_filter_selection, with=F]
-  DT::datatable(plotdata,
+  datatable(plotdata,
                 filter = list(
                   position = 'top', 
                   clear = FALSE, 
@@ -135,7 +159,7 @@ output$metadata_table <- DT::renderDataTable({
                 options = list(
                   dom = "t",
                   pageLength = 0,
-                  sScrollY = "0px",
+                  # sScrollY = "0px",
                   autoWidth = TRUE,
                   columnDefs = list(list(className = 'dt-left', targets = "_all"))
                   )
@@ -151,7 +175,7 @@ heatmap.selected <- reactive({
     setkey(selected_rows, "contig.id")
   } else {
     x_axis <- req(input$metadata_stratify_selection)
-    y_axis <- "annotation"
+    y_axis <- req(input$annotation_stratify_selection)
     
     selected <- selection_data[,.N, by=.(get(x_axis),get(y_axis))]
     names(selected) <- c(x_axis, y_axis, 'N')
